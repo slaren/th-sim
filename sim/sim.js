1519c03b13ba7547b605fba9371d9134268caf53
slaren <ddevesa@gmail.com>
2013-02-25 12:05:59 +0100

    fixed bug in the targetting logic moved ui js to a new file so that only the simulator core is left in sim.js

--- sim/sim.js
+++ sim/sim.js
@@ -1,5 +1,3 @@
-(function() {
-//
 function sformat(str) {
 	var args = arguments;
 	return str.replace(/{(\d+)}/g, function(match, number) { 
@@ -280,7 +278,7 @@ simulator.prototype = {
 		var ai = this.get_unit_info(a_unit);
 		var di = this.get_unit_info(d_unit);
 
-		if (di.type == "structure")
+		if (!ai.damage_mod[d_unit] && di.type == "structure")
 			return ai.damage_mod["Structures"];
 		else
 			return ai.damage_mod[d_unit];
@@ -307,7 +305,8 @@ simulator.prototype = {
 				// choose by range
 				(a_unit_lv.range < mt_unit_lv.position && a_unit_lv.range >= d_unit_lv.position) ||
 				// choose by damage modifier
-				(this.get_damage_mod(a_stack.unit, d_stack.unit) > this.get_damage_mod(a_stack.unit, mt_stack.unit))) {
+				((a_unit_lv.range >= d_unit_lv.position || d_unit_lv.position <= mt_unit_lv.position) &&
+				(this.get_damage_mod(a_stack.unit, d_stack.unit) > this.get_damage_mod(a_stack.unit, mt_stack.unit)))) {
 
 				mt_stack = d_stack;
 				mt_unit = d_unit;
@@ -336,7 +335,8 @@ simulator.prototype = {
 							// choose by range
 							(a_unit_lv.range < t_unit_lv.position && a_unit_lv.range >= d_unit_lv.position) ||
 							// choose by damage modifier
-							(this.get_damage_mod(a_stack.unit, d_stack.unit) > this.get_damage_mod(a_stack.unit, t_stack.unit))) {
+							((a_unit_lv.range >= d_unit_lv.position || d_unit_lv.position <= t_unit_lv.position) &&
+							(this.get_damage_mod(a_stack.unit, d_stack.unit) > this.get_damage_mod(a_stack.unit, t_stack.unit)))) {
 							t_stack = d_stack;
 							t_unit = d_unit;
 							t_unit_lv = d_unit_lv
@@ -459,295 +459,3 @@ simulator.prototype = {
 		return count;
 	}
 };
-
-//////////////////////////////////////////////////////////////////////////////
-// UI
-//////////////////////////////////////////////////////////////////////////////
-$(function() {
-	var sim = new simulator();
-	window.sim = sim;
-	sim.ready(function() {
-		var attacker = [];
-		var defender = [];
-
-		sim.set_combat_callbacks(
-			// damage
-			function(stack, t_stack, damage, is_attacker) {
-				if (t_stack) {
-					if (is_attacker) {
-						$("#battle_log").append(sformat("<div class='log_line log_damage'><img class='log_unit_image' src='{7}'> {5}x{6} &lt;= {4} &lt;= <img class='log_unit_image' src='{3}'> {1}x{2}</div>", 
-							stack.unit,
-							stack.count,
-							sim.get_unit_info(stack.unit).image,
-							damage.toFixed(1),
-							t_stack.unit,
-							t_stack.count,
-							sim.get_unit_info(t_stack.unit).image));
-					}
-					else {
-						$("#battle_log").append(sformat("<div class='log_line log_damage'><img class='log_unit_image' src='{3}'> {1}x{2} =&gt; {4} =&gt; <img class='log_unit_image' src='{7}'> {5}x{6}</div>", 
-							stack.unit,
-							stack.count,
-							sim.get_unit_info(stack.unit).image,
-							damage.toFixed(1),
-							t_stack.unit,
-							t_stack.count,
-							sim.get_unit_info(t_stack.unit).image));
-					}
-				}
-				else {
-					$("#battle_log").append("<br>");
-				}
-			},
-			// stack death
-			function(stack, is_attacker) {
-				$("#battle_log").append(sformat("<div class='log_defeat'>{1} ({2}) was defeated</div>", stack.unit, is_attacker ? "attacker" : "defender"));
-			})
-
-		// ui stuff
-		var current_army;
-		var prev_ser_state;
-
-		function update_url() {
-			prev_ser_state = serialize_state();
-			window.location.hash = encodeURIComponent(prev_ser_state);
-		};
-
-		function update_from_url() {
-			var data = decodeURIComponent(window.location.hash.substr(1));
-			if (data != prev_ser_state) {
-				prev_ser_state = data;
-				deserialize_state(data);
-				reset_battle();
-			}
-		}
-
-		function serialize_state() {
-			return sformat("{1}-{2}", serialize_army(attacker), serialize_army(defender));
-		}
-
-		function serialize_army(army) {
-			var strs = [];
-			for (var i in army) {
-				var unit = army[i];
-				strs.push(sformat("{1}{2}x{3}", unit.unit, unit.level, unit.count))
-			}
-			return strs.join("~");
-		}
-
-		function deserialize_state(data) {
-			var data_armies = data.split("-");
-			if (data_armies.length == 2) {
-				attacker = deserialize_army(data_armies[0]);
-				defender = deserialize_army(data_armies[1]);
-			}
-		}
-
-		function deserialize_army(army_data) {
-			var re = /([^\d]+)(\d+)x(\d+)/
-			var units = army_data.split("~");
-			var army = []
-
-			for (var i in units) {
-				var unit_data = units[i];
-				var result = re.exec(unit_data);
-
-				if (result && result.length == 4) {
-					var unit = {
-						unit: result[1],
-						level: parseInt(result[2], 10),
-						count: parseInt(result[3], 10)
-					};
-
-					var info = sim.get_unit_info(unit.unit);
-					if (info != null && unit.level >= 1 && info.levels && info.levels[unit.level - 1] != null &&
-						unit.count > 0 && unit.count < 1000000) {
-						army.push(unit);
-					}
-				}
-			}
-			return army;
-		}
-
-		function display_stacks(stacks, div) {
-			div.empty();
-			for (var i = 0; i < stacks.length; ++i) {
-				var stack = stacks[i];
-				div.append(sformat("<div class='unit_stack' title='{1} (lv.{2}) (hp: {5}/{6} damage done: {7})'><img class='unit_stack_image' src='{4}'><span class'unit_stack_text'>{3}</span></div>",
-				    stack.unit, stack.level, stack.count, sim.get_unit_info(stack.unit).image, 
-					stack.hp.toFixed(1), stack.hp_max.toFixed(1), stack.damage_dealt.toFixed(1)));
-			}
-		};
-
-		function close_toplevel_dialog() {
-			if ($("#army_add_dialog:visible").length > 0) {
-				$("#army_add_dialog").hide();
-				$("#fullscreen_modal_background").css("z-index", 1);
-			}
-			else {
-				$("#army_dialog").hide();
-				$("#fullscreen_modal_background").hide();
-			}
-		};
-
-		function show_army_dialog(army, is_attacker) {
-			current_army = army;
-			$("#army_dialog_type").text(is_attacker ? "Attacker" : "Defender");
-			$("#fullscreen_modal_background").css("z-index", 1);
-			$("#fullscreen_modal_background").show();
-			populate_army_dialog_army(army);
-			$("#army_dialog").show();
-		};
-
-		function populate_army_dialog_army(army) {
-			$("#army_dialog_army_unit_list").empty();
-			for (var i in army) {
-				var unit = army[i];
-				var el = $(sformat("<div class='unit_stack' title='{1} (lv.{2})'><img class='unit_stack_image' src='{4}'><span class'unit_stack_text'>{3}</span></div>", 
-					unit.unit, unit.level, unit.count, sim.get_unit_info(unit.unit).image));
-
-				var button = $("<button>x</button>");
-				el.prepend(button)
-				$("#army_dialog_army_unit_list").append(el);
-				button.click((function(i) { return function() { remove_current_army_unit(i); }; })(i));
-			}
-
-			var cost = sim.get_army_cost(army);
-			$("#army_dialog_cost").empty().append(
-				$(sformat("<span>{1}<span class='resource-labor'></span> {2}<span class='resource-gold'></span> {3}<span class='resource-wood'></span> {4}<span class='resource-crop'></span> {5}<span class='resource-iron'></span></span>", cost.labor, cost.gold, cost.wood, cost.crop, cost.iron)));
-			$("#army_dialog_upkeep").text(cost.upkeep);
-			
-			update_url();
-		};
-
-		function show_army_add_dialog(unit) {
-			// fill level drop down
-			$("#army_add_dialog_unit_level").empty();
-			for (var i = 1; i <= sim.get_unit_info(unit).levels.length; ++i) {
-				var el = $(sformat("<option value='{1}'>{1}</option>", i));
-				$("#army_add_dialog_unit_level").append(el);
-			}
-			$("#fullscreen_modal_background").css("z-index", 2);
-			$("#army_add_dialog_unit_name").text(unit);
-			$("#army_add_dialog").show();
-		}
-
-		function add_current_army_unit(unit, level, count) {
-			var unitdef = {
-				unit: unit,
-				level: level,
-				count: count	
-			};
-			current_army.push(unitdef);
-			populate_army_dialog_army(current_army);
-			reset_battle();
-		}
-
-		function remove_current_army_unit(index) {
-			current_army.splice(index, 1);
-			populate_army_dialog_army(current_army);
-			reset_battle();
-		}
-
-		// load state
-		update_from_url();
-
-		// fill available unit list
-		for (var i in sim.unit_db) {
-			var unit = sim.unit_db[i];
-			if (unit.name) {
-				var button = $(sformat("<div class='army_available_unit'><img class='army_available_unit_image' src='{2}'><br>{1}</div>", unit.name, unit.image));
-				button.click((function(unit) { return function() { show_army_add_dialog(unit.name); } })(unit));
-
-				$(unit.type == "structure" ? "#army_dialog_available_structure_list" : "#army_dialog_available_unit_list").append(button); 
-			}
-		}
-
-		// battle stuff
-		var current_round = 1;
-
-		function reset_battle() { 
-			sim.set_attacker(attacker);
-			sim.set_defender(defender);
-
-			display_stacks(sim.get_attacker_stacks(), $("#attacker"));
-			display_stacks(sim.get_defender_stacks(), $("#defender"));
-			
-			$("#battle_log").empty();
-			
-			current_round = 1;
-		}
-
-		function run_round() {
-			$("#battle_log").append(sformat("<div class='log_round_header'>Round {1}</div>", current_round))
-			sim.do_round();
-
-			display_stacks(sim.get_attacker_stacks(), $("#attacker"));
-			display_stacks(sim.get_defender_stacks(), $("#defender"));
-
-			current_round++;
-		}
-
-		reset_battle();
-
-		// ui callbacks
-		$("#reset_button").click(function() {
-			reset_battle();
-		});
-
-		$("#play_all_button").click(function() {
-			if (current_round > 20 || !sim.is_in_combat())
-				reset_battle();
-
-			while (current_round <= 20 && sim.is_in_combat()) {
-				run_round();
-			}
-			$("#battle_log").animate({ scrollTop: $("#battle_log")[0].scrollHeight }, "fast");
-		});
-
-		$("#next_round_button").click(function() {
-			if (sim.is_in_combat()) {
-				run_round();
-				$("#battle_log").animate({ scrollTop: $("#battle_log")[0].scrollHeight }, "fast");
-			}
-		});
-
-		$("#edit_attacker_button").click(function() {
-			show_army_dialog(attacker, true);
-		});
-
-		$("#edit_defender_button").click(function() {
-			show_army_dialog(defender, false);
-		});
-
-		$("#army_dialog_close_button").click(function() {
-			close_toplevel_dialog();
-		});
-
-		$("#army_add_dialog_add_button").click(function() {
-			var name = $("#army_add_dialog_unit_name").text();
-			var level = $("#army_add_dialog_unit_level").val();
-			var count = $("#army_add_dialog_unit_amount").val();
-
-			add_current_army_unit(name, level, count);
-			close_toplevel_dialog();
-		});
-
-		$("#army_add_dialog_cancel_button").click(function() {
-			close_toplevel_dialog();
-		});
-
-		$(window).on("hashchange", function() {
-			update_from_url();
-		});
-
-		$(document).keydown(function(e) {
-			// esc
-			if (e.which == 27) {
-				close_toplevel_dialog();
-			}
-		});
-	});
-});
-//
-})();
